06f7c2e5c5087239e5e9eace32b853d86ee1bf46
Aaron <rexforddrive@gmail.com>
2017-09-29 00:34:11 -0700

    Create MyList.java

---
+++ LinkedList/MyList.java
@@ -0,0 +1,7 @@
+public interface MyList {
+  public void add(String str);
+  public boolean contains(String str);
+  public boolean remove(String str);
+  public String get(int index);
+  public int size();
+}
