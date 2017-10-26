7c93d20a1f6c983d59fbdfd5fc1194851b69a4bb
Aaron <rexforddrive@gmail.com>
2017-10-26 14:40:50 -0700

    added insertAt method

--- LinkedList/MyLinkedList.java
+++ LinkedList/MyLinkedList.java
@@ -53,6 +53,46 @@ public class MyLinkedList implements MyList {
     return null;
   }
   
+  public boolean insertAt(String value, int index) {
+    if(head == null && index != 0) {
+      System.out.println("Error. List has not been created yet. Either call add method or insert with 0 as index.");
+      return false;
+    }
+    if(index < 0 || index >= this.size()) {
+      System.out.println("Error. Index out of range.");
+      return false;
+      
+    }
+    Node newNode = new Node(value);
+    if(index == 0) {
+      newNode.next = head;
+      head = newNode;
+    }
+    
+    else if(index == this.size() - 1) {
+      tail.next = newNode;
+      tail = tail.next;
+    }
+    
+    else {
+      Node currentNode = head;
+      int currentIndex = 0;
+      while(currentNode != null) {
+        if(index == currentIndex + 1) {
+          break;
+        }
+        currentIndex++;
+        currentNode = currentNode.next;
+      }
+      Node next = currentNode.next;
+      currentNode.next = newNode;
+      currentNode.next.next = next;
+      
+    }
+    
+    return true;
+  }
+  
   public int size() {
     int sizeCount = 0;
     if(head == null) {
